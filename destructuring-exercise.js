1a8e74d220abb249495ef5de71505d62b1e18a17
Daniel Bae <danibae91@icloud.com>
2021-02-20 23:23:03 -0800

    finish exercise

--- destructuring-exercise.js
+++ destructuring-exercise.js
@@ -96,3 +96,29 @@ const obj = {
 const {a, b} = obj.numbers 
 
 // ES5 Array Swap
+var arr = [1, 2];
+var temp = arr[0];
+arr[0] = arr[1];
+arr[1] = temp;
+
+// ES2015 One-Line Array Swap with Destructuring
+/* Write an ES2015 Version */
+[arr[0], arr[1]] = [arr[1], arr[0]]
+
+// raceResults()
+/** Write a function called raceResults which accepts a single array argument. It should return an object with the keys first, second, third, and rest.
+
+first: the first element in the array
+second: the second element in the array
+third: the third element in the array
+rest: all other elements in the array */ 
+
+/** Write a one line function to make this work using
+An arrow function
+Destructuring
+‘Enhanced’ object assignment (same key/value shortcut) */ 
+raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre'])
+
+// {first: "Tom", second: "Margaret", third: "Allison", rest: ["David", "Pierre"]}
+
+const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest})
